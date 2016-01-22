805e303b71b1fe82e5633db09a23c588b670eac0
Lauren Rouse <lauren.l.rouse@gmail.com>
2016-01-21 21:26:40 -0600

    formatting page nearly formatted

--- app/app.module.js
+++ app/app.module.js
@@ -1,1 +1,5 @@
-var app = angular.module("osm-nashville", ['ngRoute', 'firebase', 'ui-leaflet', 'ngMaterial']);
+var app = angular.module("osm-nashville", ['ngRoute', 'firebase', 'ui-leaflet', 'ngMaterial'])
+.config(function($mdThemingProvider) {
+  $mdThemingProvider.theme('altTheme')
+    .primaryPalette('yellow')
+});
