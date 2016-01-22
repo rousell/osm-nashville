6eba50ee064c8e4869c703125de9095cae4cfa54
Lauren Rouse <lauren.l.rouse@gmail.com>
2016-01-21 22:17:22 -0600

    some more work on login

--- app/app.module.js
+++ app/app.module.js
@@ -1,5 +1,6 @@
 var app = angular.module("osm-nashville", ['ngRoute', 'firebase', 'ui-leaflet', 'ngMaterial'])
 .config(function($mdThemingProvider) {
-  $mdThemingProvider.theme('altTheme')
-    .primaryPalette('yellow');
+  $mdThemingProvider.theme('altTheme', 'default')
+    .primaryPalette('yellow')
+    .dark();
 });
