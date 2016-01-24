0c7957532719f175be3e806c3e0e86545658996f
Lauren Rouse <lauren.l.rouse@gmail.com>
2016-01-24 16:06:45 -0600

    navbar converted to angular material toolbar

--- app/app.module.js
+++ app/app.module.js
@@ -1,6 +1,6 @@
 var app = angular.module("osm-nashville", ['ngRoute', 'firebase', 'ui-leaflet', 'ngMaterial'])
 .config(function($mdThemingProvider) {
-  $mdThemingProvider.theme('default')
-    .primaryPalette('yellow')
-    .accentPalette('brown');
+  // $mdThemingProvider.theme('default')
+  //   .primaryPalette('yellow')
+  //   .accentPalette('brown');
 });
